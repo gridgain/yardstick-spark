60ceb371bdd60dae349ee763abf1597cd7ceab62
sboeschhuawei <stephen.boesch@huawei.com>
2015-06-21 23:11:22 -0700

    Added scala and spark to the pom.xml and a couple of basic classes. Has a compilation error still working on it

---
+++ src/main/scala/org/yardstickframework/spark/SparkAbstractBenchmark.scala
@@ -0,0 +1,39 @@
+/*
+ Licensed under the Apache License, Version 2.0 (the "License")
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package org.yardstickframework.spark
+
+import org.yardstickframework._
+
+import org.apache.spark._
+
+/**
+ * Hazelcast benchmark that performs put and query operations.
+ */
+class SparkAbstractBenchmark(cacheName: String) extends BenchmarkDriverAdapter { 
+   var sc : SparkContext = _
+   @throws(classOf[Exception])
+   override def setUp(cfg: BenchmarkConfiguration) {
+     super.setUp(cfg)
+     sc = new SparkContext("local[4]", "BenchmarkTest")
+   }
+
+   @throws(classOf[Exception])
+   override def tearDown() {
+     super.setUp(cfg)
+     sc.stop
+   }
+
+}
+
