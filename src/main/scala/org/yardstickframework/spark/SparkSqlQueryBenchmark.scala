5c85cbaf0f179c4dd14855f5bc238a3223873f7c
sany <sany@sany.(none)>
2015-06-23 20:49:10 +0530

    Timeit start and Timeit ended

--- src/main/scala/org/yardstickframework/spark/SparkSqlQueryBenchmark.scala
+++ src/main/scala/org/yardstickframework/spark/SparkSqlQueryBenchmark.scala
@@ -14,9 +14,15 @@
 
 package org.yardstickframework.spark
 
+import com.yardstickframework.model.Sensor
 import org.yardstickframework._
+import com.yardstickframework.util.TimerArray
+
 
 class SparkSqlQueryBenchmark extends SparkAbstractBenchmark("query") {
+
+  val timer = new TimerArray
+
    @throws(classOf[Exception])
    override def setUp(cfg: BenchmarkConfiguration) {
      super.setUp(cfg)
@@ -24,11 +30,24 @@ class SparkSqlQueryBenchmark extends SparkAbstractBenchmark("query") {
 
    @throws(classOf[java.lang.Exception])
    override def test(ctx: java.util.Map[AnyRef, AnyRef]) : Boolean =  {
-      val rdd = sc.parallelize( (0 until 1000).toSeq)
-      val mrdd = rdd.sum
-      println(s"Is the n*(n+1)/2 formula working? $mrdd")
+     val rdd = sc.textFile("/home/sany/Downloads/TruckEvents.csv")
+     val runResults = timer("Sensor-Data") {
+       val mrdd = rdd.map(line=>new Sensor(line.split(",")(0).toInt,line.split(",")(1).toInt,line.split(",")(2).toDouble,
+         line.split(",")(3).toDouble,line.split(",")(4).toInt,line.split(",")(5).toInt,line.split(",")(6).toInt,line.split(",")(7).toInt,
+         line.split(",")(8).toInt,line.split(",")(9).toInt))
+       println(mrdd.count())
+     }
+   //  println(s"Runner finished. Got results for ${runResults} features:")
+     //runResults foreach { runner =>
+     //  println(runner.mkString(", "))
+     //}
+    // println(timer.toString("Running time: "))
+   //  runResults
+
+
       true
    }
+
 }
 
 object SparkSqlQueryBenchmark {
