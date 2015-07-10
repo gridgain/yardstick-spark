36a79815794854cb1c60a57070dadacdf2239239
sboeschhuawei <stephen.boesch@huawei.com>
2015-07-10 09:57:55 -0700

    small cleanup

--- src/main/scala/org/yardstickframework/spark/CoreBattery.scala
+++ src/main/scala/org/yardstickframework/spark/CoreBattery.scala
@@ -1,16 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.yardstickframework.spark
 
 import java.text.SimpleDateFormat
 import java.util.Date
 
-import org.apache.ignite.spark.IgniteContext
 import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
 import org.yardstickframework.spark.YsSparkTypes._
-import beans.BeanProperty
 import org.yardstickframework.spark.util.YardstickLogger._
-import YsSparkTypes._
-import collection.mutable
+
+import scala.collection.mutable
 
 case class TestResult(testName: String, resultName: String,
   optCount: Option[Int] = None, optResult: Option[Any] = None, stdOut: Option[String] = None,
--- src/main/scala/org/yardstickframework/spark/DataGenerator.scala
+++ src/main/scala/org/yardstickframework/spark/DataGenerator.scala
@@ -1,14 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.yardstickframework.spark
 
 import java.io.Serializable
 
-import org.apache.ignite.spark.IgniteContext
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
 import org.yardstickframework.spark.YsSparkTypes._
-import collection.mutable
-import scala.util.Random
-import org.yardstickframework.spark.util.YardstickLogger._
+
+import scala.collection.mutable
 
 object YsSparkTypes {
   type RddKey = Long
@@ -19,38 +33,6 @@ object YsSparkTypes {
   type AggRDD = RDD[(RddKey, Iterable[RddVal])]
   type CountRDD = RDD[(RddKey, Long)]
 
-  /**
-  abstract case class ProbDistFn()() {
-    def genOne(min: Long, max: Long): Double
-  }
-
-  case class EqualRangesPartitioner(min: Long, max: Long, override val numPartitions: Int) extends Partitioner {
-    assert(max - min > 0, "Hi! Please try to make your max greater than your min. Ten-four.")
-    assert(numPartitions > 0, "Hi! Black-hole style partitions are disallowed at this time.")
-    val psize = ((max - min) / numPartitions).toInt
-    val partitions = for (p <- 0 until numPartitions) yield {
-
-    }
-
-    override def getPartition(key: Any): Int = {
-      assert(key.isInstanceOf[Long], "Let us get some LONG keys please")
-      val pnum = key.asInstanceOf[Long] % psize
-      assert(pnum < Int.MaxValue, s"Houston, we have an uncooperative key (> MaxInt) here: $pnum")
-      pnum.toInt
-    }
-  }
-
-  case class SkewGen(sc: SparkContext, nPartitions: Int, nRecords: Int, namePrefix: String,
-    min: Long, max: Long, probFn: ProbDistFn) {
-    def genData() = {
-      sc.parallelize(for (rx <- 0 until nRecords) yield {
-        val rec = (s"$namePrefix-$rx", probFn.genOne(min, max))
-        rec
-      }, nPartitions)
-    }.partitionBy(EqualRangesPartitioner(min, max, nPartitions))
-
-  }
-    * */
   sealed abstract class Action(name: String)
 
   case object Collect extends Action("collect")
@@ -133,8 +115,6 @@ class SingleSkewDataGenerator(sc: SparkContext, optIcInfo: Option[IcInfo], dataP
           var longs = new java.util.Random().longs(nrecs, lbound, ubound).iterator
           val out = (0 until nrecs).foldLeft(mutable.ArrayBuffer[RddTuple]()) { case (m, n) =>
             val windex = rnd.nextInt(locData.nWords)
-
-            // println(s"Word index = $windex")
             m += Tuple2(longs.next, locData.words(windex))
           }
           out
