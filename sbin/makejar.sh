601bf194f2015c2e15f5c914de2f38b8650022f7
javadba <javadba@gmail.com>
2015-09-08 21:02:39 -0700

    update makejar.sh to support 1.5.0

--- sbin/makejar.sh
+++ sbin/makejar.sh
@@ -8,17 +8,18 @@ cd $curdir
 
 JARS=$(echo "./libs/yardstick-spark-0.0.1.jar
 ./repo/ignite-yardstick-0.1.0.jar
-./repo-aws/ignite-aws-1.4.1-SNAPSHOT.jar
-./repo-core/ignite-core-1.4.1-SNAPSHOT.jar
-./repo-indexing/ignite-indexing-1.4.1-SNAPSHOT.jar
-./repo-log4j/ignite-log4j-1.4.1-SNAPSHOT.jar
-./repo-scalar/ignite-scalar-1.4.1-SNAPSHOT.jar
-./repo-spring/ignite-spring-1.4.1-SNAPSHOT.jar
-./repo-spark/ignite-spark-1.4.1-SNAPSHOT.jar
+./repo-aws/ignite-aws-1.5.0-SNAPSHOT.jar
+./repo-core/ignite-core-1.5.0-SNAPSHOT.jar
+./repo-indexing/ignite-indexing-1.5.0-SNAPSHOT.jar
+./repo-log4j/ignite-log4j-1.5.0-SNAPSHOT.jar
+./repo-scalar/ignite-scalar-1.5.0-SNAPSHOT.jar
+./repo-spring/ignite-spring-1.5.0-SNAPSHOT.jar
+./repo-spark/ignite-spark-1.5.0-SNAPSHOT.jar
 ./libs/aws-java-sdk-1.3.21.1.jar
 " | tr '\n' ' ')
 for jar in $JARS; do cp $jar $jdir; done
 for jar in libs/*; do echo $jar; cp $jar $jdir; done
+for jar in libs2/*; do echo $jar; cp $jar $jdir; done
 for jar in install_libs/*; do echo $jar; cp $jar $jdir; done
 cd $jdir
 for x in *.jar; do jar -xvf $x; done
